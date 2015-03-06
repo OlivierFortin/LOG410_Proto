81fa38a9f20694bd328c82d72eacf2a48d077305
Olivier Fortin <olivierf34@gmail.com>
2015-03-06 14:35:26 -0500

    new adata

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,108 @@
-console.log('\'Allo \'Allo!');
+
+$(function () {
+    var altitude = 0;
+    var speed = 30;
+    var isDown = 0;
+    $(document).ready(function () {
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+
+        $('#highcharts').highcharts({
+            chart: {
+                type: 'spline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function () {
+
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        var series2 = this.series[1];
+                        setInterval(function () {
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.random()*100;
+                            if(speed <= 0 ) {
+                               altitude -= 3;
+                            }else {
+                               altitude += 3;
+                           }
+                           if (altitude <= 0) {
+                                altitude = 0;
+                           }
+                           if(speed <= 0) speed = 0;
+                           else speed += -1;
+                            series.addPoint([x, speed], true, true);
+                            series2.addPoint([x,altitude],true, true);
+                        }, 1000);
+
+                    }
+                }
+            },
+            title: {
+                text: 'Pression'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Pression'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 30);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Speed',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -19; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: 30
+                        });
+                    }
+                    return data;
+                }())
+            },{
+                name: 'Altitude',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -19; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: 0
+                        });
+                    }
+                    return data;
+                }())}]
+        });
+    });
+});
